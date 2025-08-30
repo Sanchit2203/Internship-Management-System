fc479f253974b7193bcbc83d3a329ea219579337
sanchitsinha14 <sanchitsinha14@gmail.com>
2025-08-30 11:33:47 +0000

    v2.0

--- score.js
+++ score.js
@@ -2,5 +2,5 @@
 function calculateScore(tenthMarks, twelfthMarks, cgpa) {
   // The formula is: (10% / 10 + 12% / 10 + cgpa) / 3
   const score = (tenthMarks / 10 + twelfthMarks / 10 + cgpa) / 3;
-  return score;
+  return score.toFixed(2);
 }
